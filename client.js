cc4933921c6e6b4c7a57868da8f256a74d084c63
ZhenyaMartynov <146564992+ZhenyaMartynov@users.noreply.github.com>
2023-12-16 18:28:13 +0700

    Update client.js

--- client.js
+++ client.js
@@ -1,7 +1,12 @@
 Teams.Add('Test', 'Test', { r: 0 });
 
 Teams.OnPlayerChangeTeam.Add(function (p) { p.Spawns.Spawn(); });
-Teams.OnRequestJoinTeam.Add(function (p, t) { t.Add(p); });
+Teams.OnRequestJoinTeam.Add(function (p, t) { 
+   t.Add(p); 
+  try {
+    menu.ItemsService.SkyColor = { r: 1 };
+  } catch (err) { msg.Show(err); }
+});
 
 let a = AreaPlayerTriggerService.Get('A');
 a.Tags = ['a'];
